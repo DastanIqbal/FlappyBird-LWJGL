7af8bfc5b935ac6800f4b83970ed116ed6541a3d
Iqbal Ahmed (Sinbad) <ask2iqbal@gmail.com>
2018-09-23 22:41:03 +0800

    bump lwjgl 3 and created blank window

--- src/com/flappy/game/Main.kt
+++ src/com/flappy/game/Main.kt
@@ -1,9 +1,9 @@
 package com.flappy.game
 
-import com.flappy.game.graphics.Shader
-import com.flappy.game.math.Vector3f
-import org.lwjgl.opengl.*
-import org.lwjgl.opengl.GL11.*
+import org.lwjgl.glfw.GLFW
+import org.lwjgl.opengl.GL11
+import org.lwjgl.system.MemoryUtil.NULL
+
 
 /**
  * Created by dastaniqbal on 23/09/2018.
@@ -14,53 +14,53 @@ class Main : Runnable {
 
     val width = 640//1280
     val height = 480
-    val title = "Flappy"
+
     var running = false
     var mainThread: Thread? = null
+    var window = 0L
 
     fun start() {
         running = true
-        mainThread = Thread(this, "Display")
+        mainThread = Thread(this, "Game")
         mainThread?.start()
     }
 
     private fun init() {
-        val version = glGetString(GL_VERSION)
-        println(version)
-        glClearColor(1.0f, 1.0f, 1.0f, 1.0f)
+        if (!GLFW.glfwInit()) {
+            //TODO: handle it
+        }
 
-        Shader.loadAll()
+        GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GL11.GL_TRUE)
+        window = GLFW.glfwCreateWindow(width, height, "Flappy", NULL, NULL)
+        if (window == NULL) {
+            //TODO: handle it
+            return
+        }
+
+        val vidmode = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor())
+        GLFW.glfwSetWindowPos(window, (vidmode?.width()!! - width) / 2, (vidmode.height() - height) / 2)
+
+        GLFW.glfwMakeContextCurrent(window)
+        GLFW.glfwShowWindow(window)
     }
 
     override fun run() {
-        Display.setDisplayMode(DisplayMode(width, height))
-        Display.setTitle(title)
-        val context = if (System.getProperty("os.name") == "Mac")
-            ContextAttribs(3, 2)
-        else
-            ContextAttribs(3, 3)
-        Display.create(PixelFormat(), context.withProfileCore(true))
-
         init()
-
-        val vao = GL30.glGenVertexArrays()
-        GL30.glBindVertexArray(vao)
-
-        val shader = Shader.BASIC
-        shader.enable()
-        shader.setUniform3f("col", Vector3f(0.8f, 0.2f, 0.3f))
-
         while (running) {
+            update()
             render()
-            Display.update()
-            if (Display.isCloseRequested()) running = false
+
+            if (GLFW.glfwWindowShouldClose(window))
+                running = false
         }
-        Display.destroy()
+    }
+
+    fun update() {
+        GLFW.glfwPollEvents()
     }
 
     fun render() {
-        glClear(GL_COLOR_BUFFER_BIT)
-        glDrawArrays(GL_TRIANGLES, 0, 3)
+        GLFW.glfwSwapBuffers(window)
     }
 }
 
